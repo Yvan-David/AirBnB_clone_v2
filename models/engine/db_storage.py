1211b04a674a9b97ddb695bfe5a4c904d498015a
M'mah Zombo <110876378+Mmah-Zombo@users.noreply.github.com>
2023-04-18 02:35:26 +0100

    Create db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""data base storage"""
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from os
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+
+class DBStorage:
+  """a class that represebts the database"""
+  __engine = None
+  __session = None
+  
+  def __init__(self):
+    user = os.environ.get('HBNB_MYSQL_USER')
+    pwd = os.environ.get('HBNB_MYSQL_PWD')
+    host = os.environ.get('HBNB_MYSQL_HOST', 'localhost')
+    db = os.environ.get('HBNB_MYSQL_DB')
+    self.__engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@{host}/{db}',
+                                       pool_pre_ping=True)
+    if os.environ.get('HBNB_ENV') == 'test':
+      Base.metadata.drop_all(self.__engine)
