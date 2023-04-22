2c6ee5e6407e8db05f5d0057ff847e7c5904c2c9
Yvan-David <irankundayvan2020@gmail.com>
2023-04-22 13:53:17 +0200

    prob fixing task6

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column,ForeignKey, String
+from sqlalchemy.orm import relationship
 from os import getenv
 
 class City(BaseModel, Base):
@@ -12,7 +13,9 @@ class City(BaseModel, Base):
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), nullable=False, Foreignkey=True) 
+        state_id = Column(String(60),ForeignKey('states_id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
     else:
         state_id = ""
         name = ""
