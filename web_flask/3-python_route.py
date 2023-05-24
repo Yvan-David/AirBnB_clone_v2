d0372752a39cfdbe87da297831f62f49ab320cb8
Yvan-David <irankundayvan2020@gmail.com>
2023-05-24 17:09:07 +0200

    flask starting

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,47 +2,46 @@
 """ Starting a flask web application """
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello():
-    """prints hello on the web """
-    return 'Hello HBNB!'
+    """prints hello on the web."""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
-    """prints hello on the web """
-    return 'HBNB'
+    """prints hello on the web."""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def hello_text(text):
-    """prints hello on the web """
+@app.route("/c/<text>", strict_slashes=False)
+def tet(text):
+    """prints hello on the web."""
     result = text.replace("_", " ")
-    return f'C {result}'
+    return "C {}".format(text)
 
 
-@app.route('/c', strict_slashes=False)
+@app.route("/c", strict_slashes=False)
 def hello_pytext():
-    """prints hello on the web """
-    return 'c is cool'
+    """prints hello on the web."""
+    return "c is cool"
 
 
-@app.route('/python', strict_slashes=False)
+@app.route("/python", strict_slashes=False)
 def hello_pytext():
-    """prints hello on the web """
-    return f'Python is cool'
+    """prints hello on the web."""
+    return "Python is cool."
 
 
-@app.route('/python/<text>', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def hello_pythtext(text):
-    """prints hello on the web """
+    """prints hello on the web."""
     result = text.replace("_", " ")
-    return f'Python {result}'
+    return "Python {}".format(result)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
