a15d606b001e7c4a79bd2d57cfc7643f500d770a
Yvan-David <irankundayvan2020@gmail.com>
2023-05-24 13:01:24 +0200

    flask starting an app

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Starting a flask web application """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """prints hello on the web """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """prints hello on the web """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_text(text):
+    """prints hello on the web """
+    result = text.replace("_", " ")
+    return f'C {result}'
+
+
+@app.route('/c', strict_slashes=False)
+def hello_pytext():
+    """prints hello on the web """
+    return 'c is cool'
+
+
+@app.route('/python', strict_slashes=False)
+def hello_pytext():
+    """prints hello on the web """
+    return f'Python is cool'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def hello_pythtext(text):
+    """prints hello on the web """
+    result = text.replace("_", " ")
+    return f'Python {result}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """prints hello on the web """
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
