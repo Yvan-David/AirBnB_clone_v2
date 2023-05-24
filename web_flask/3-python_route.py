7f7e3e6c5e7f9e397fa29f86b714e4201288fdb0
Yvan-David <irankundayvan2020@gmail.com>
2023-05-24 17:22:11 +0200

    flask startin

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
+"""Starts a Flask web application.
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
     /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
 """
 from flask import Flask
 
@@ -14,53 +14,36 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    """
-    prints hello on the web.
-    """
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hello_hbnb():
-    """
-    prints hello on the web.
-    """
+def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def tet(text):
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
-    prints hello on the web.
-    """
-    result = text.replace("_", " ")
+    text = text.replace("_", " ")
     return "C {}".format(text)
 
 
-@app.route("/c", strict_slashes=False)
-def hello_pytext():
-    """
-    prints hello on the web.
-    """
-    return "c is cool"
-
-
 @app.route("/python", strict_slashes=False)
-def hello_pytext():
-    """
-    prints hello on the web.
-    """
-    return "Python is cool."
-
-
 @app.route("/python/<text>", strict_slashes=False)
-def hello_pythtext(text):
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
-    prints hello on the web.
-    """
-    result = text.replace("_", " ")
-    return "Python {}".format(result)
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 if __name__ == "__main__":
